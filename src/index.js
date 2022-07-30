3df17916199bf63fb3f2dd3b724371f401a40e84
cystal-dot <jiniaen4@gmail.com>
2022-07-31 01:03:42 +0900

    ボタン描画

--- src/index.js
+++ src/index.js
@@ -1,17 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button className='Button'>
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Calclator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result : 0,
+      }
+    }
+
+  renderButton(params) {
+    return (
+      <Button 
+        value = {params}
+        onClick = { () => this.handleClick(params)}
+      />
+    )
+  }
+
+  handleClick(i) {
+    console.log("test")
+    this.setState({
+      result : this.state.result+i,
+    })
+  }
+
+  renderResult() {
+    return (
+      <text className='Result'>
+        {this.state.result}
+      </text>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderResult()}
+        <div className='Button'>
+          <div className='line1'>
+            {this.renderButton(1)}
+            {this.renderButton(2)}
+            {this.renderButton(3)}
+          </div>
+          <div className='line2'>
+            {this.renderButton(4)}
+            {this.renderButton(5)}
+            {this.renderButton(6)}
+          </div>
+          <div className='line3'>
+            {this.renderButton(7)}
+            {this.renderButton(8)}
+            {this.renderButton(9)}
+          </div>
+            {this.renderButton(0)}
+            {this.renderButton(0)}
+            {this.renderButton(0)}
+        </div>
+      </div>
+    );
+  }
+}
+
+// root.render ========================================
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Calclator />);
